2cc592db5105f6ca93932437c33355b8c5dfbdcf
Jose Sulbaran <jose.sulbaran@uno.cl>
2023-05-16 18:41:37 -0400

    websocket con sockeit.io y agregar cors

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+const cors = require('cors')
 const bodyParser = require("body-parser");
+const socket = require('./socket')
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_REST_OF_URI}`;
-
 const db = require("./db");
-
 const router = require("./network/routes");
 
 db(uri, process.env.DB_NAME);
 
-var app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
+
+socket.connect(server);
 
 router(app);
 
 app.use("/app", express.static("public"));
 
-app.listen(3000);
-
-console.log("La aplicación esta escuchando en http://localhost:3000");
+server.listen(3000, function () {
+  console.log("La aplicación esta escuchando en http://localhost:3000");
+});
